2d84cd122025a4403da552b06fffe1173de87436
Guilherme Pereira Guimarães <guilhermezemo@gmail.com>
2024-02-29 16:46:45 -0300

    Initial commit

---
+++ projeto.rb
@@ -0,0 +1,7 @@
+print "Digite seu nome: "
+nome = gets.chomp.capitalize
+print "Digite seu sobrenome: "
+sobrenome = gets.chomp.capitalize
+print "Digite sua idade: "
+idade = gets.chomp.to_i
+puts "Olá #{nome} #{sobrenome}, você tem #{idade} anos!"
